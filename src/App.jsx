96a08b24ddcd1469bdfa482dc1372e032442ff27
Karolina <karolina.limanowska@gmail.com>
2024-04-18 17:38:27 +0200

    creating a getRandomQuestions function

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import React from 'react'
+import { Questions } from './components/questions/Questions'
 
 function App() {
 
   return (
     <>
       <div className='w-full h-full flex justify-center items-center'>
-        <div className='text-2xl font-mono'>TailwindText</div>
+        <div className='text-2xl font-mono'><Questions /></div>
       </div>
     </>
   )
