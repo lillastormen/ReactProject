6d0590397f34d6911890e2c7337ca5eedf466cd0
wlanhage <wlanhage@gmail.com>
2024-04-22 22:51:49 +0200

    added rerendering of timer on click and 10s max question time

--- src/components/CountdownBar.jsx
+++ src/components/CountdownBar.jsx
@@ -1,18 +1,24 @@
-import '../App.css'
-import React from 'react';
+import { useState, useEffect } from "react";
+import React from "react";
 
+function CountdownBar ({ countdownKey }) {
+    const [countdown, setCountdown] = useState(100); // Set initial countdown value
 
+    useEffect(() => {
+        setCountdown(100); // Reset countdown whenever countdownKey changes
+        const interval = setInterval(() => {
+            setCountdown(prevCountdown => prevCountdown - 1); // Decrease countdown every second
+        }, 1000);
+        return () => clearInterval(interval); // Clear interval on component unmount or countdownKey change
+    }, [countdownKey]);
 
-function CountdownBar () {
-
-
-    return(
-        <div  className='w-full flex justify-center'>
+    return (
+        <div className='w-full flex justify-center'>
             <div className='w-4/5 h-4 bg-gray-400 rounded-xl'>
                 <div className='h-4/5 bg-red-500 rounded-xl animate-fill'></div>
             </div>
         </div>
-    )
+    );
 }
 
 export default CountdownBar;
--- src/views/quiz.jsx
+++ src/views/quiz.jsx
@@ -2,17 +2,27 @@ import '../App.css'
 import React from 'react';
 import QuestionArea from '../components/QuestionArea.jsx';
 import CountdownBar from '../components/CountdownBar.jsx';
+import { useState, useEffect } from 'react';
 
 
 function Quiz () {
+    const [countdownKey, setCountdownKey] = useState(0);
 
+    // This function should be called whenever a new question is fetched
+    const resetTimerBar = () => {
+        setCountdownKey(prevKey => prevKey + 1);
+    };
+
+    const resetQuestionAfterTime = () => {
+        
+    }
 
     return (
         
             <div className='w-full h-full flex flex-col justify-center items-center'>
-                <CountdownBar />
+                <CountdownBar key={countdownKey} />
                 <div className='text-sm font-mono'>QuizText</div>
-                <QuestionArea />
+                <QuestionArea resetTimerBar={resetTimerBar} />
 
             </div>
             
