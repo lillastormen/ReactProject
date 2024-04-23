b2047f4f01e7a32041faa46b6bcf5f10516fcbee
Karolina <karolina.limanowska@gmail.com>
2024-04-23 22:28:48 +0200

    finishing score table

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import { getDatabase, ref, set, get, child } from "firebase/database";
 import Index from "../views";
 import Result from "../views/result";
@@ -19,6 +19,8 @@ const database = getDatabase(app);
 
 
 function Score() {
+
+    const [score, setScore] = useState();
     
     //add a user
     function writeUserData(name, result) {
@@ -32,7 +34,7 @@ function Score() {
     useEffect(() => {
         writeUserData(sessionStorage.getItem('userName'), sessionStorage.getItem('result'));
         getUserData(1);
-    })
+    },[])
     
 
     function getUserData(userId) {
@@ -40,7 +42,7 @@ function Score() {
         get(child(dbRef, `users/`))
         .then((snapshot) => {
             if (snapshot.exists()) {
-                console.log(snapshot.val());
+                setScore(snapshot.val());
             } else {
                 console.log("No data available");
             }
@@ -49,15 +51,20 @@ function Score() {
         });
     }
 
-   
-
     return (
-    <div className="w-full h-auto py-5 rounded-xl bg-gradient-to-r from-purple-400 to-blue-500 mt-12
-    hover:from-blue-500 hover:to-purple-400 ease-in-out transition-all duration-100
-    sm:w-3/5">
-    <div className='text-xl font-mono flex justify-center items-center'>Score Table</div>
-    </div>)
-}
+        <>
+            <div className="w-full h-auto py-5 rounded-xl bg-gradient-to-r from-purple-400 to-blue-500 mt-12
+            hover:from-blue-500 hover:to-purple-400 ease-in-out transition-all duration-100
+            sm:w-3/5">
+                <div className='text-xl font-mono flex justify-center items-center'>Score Table</div>
+            </div>
+                { score && Object.keys(score).map(key => (<div className='text-xl font-mono flex justify-center items-center'>{key} - {score[key].result}</div>)
+                    )
+                }
+        </>
+        )
+    }
+
 
 export default Score;
 
